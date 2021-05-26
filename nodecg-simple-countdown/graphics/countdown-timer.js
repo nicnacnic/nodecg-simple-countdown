5c5e183985e93727de94121f7f36e556f8de50f3
nicnacnic <contact@nicnacnic.com>
2021-05-25 22:25:46 -0400

    Delete nodecg-simple-countdown directory

--- nodecg-simple-countdown/graphics/countdown-timer.js
+++
@@ -1,8 +0,0 @@
-const timeLeft = nodecg.Replicant('timeLeft');
-
-NodeCG.waitForReplicants(timeLeft).then(() => {
-
-    timeLeft.on('change', (newVal, oldVal) => {
-        document.getElementById('main').innerHTML = newVal;
-    })
-})
